67267e38ca2b5ed397f6ffe9d434f4d3416aa78c
root <root@kali.kali>
2023-06-26 18:40:18 -0400

    new docker file and script.sh

--- ssh/script.sh
+++ ssh/script.sh
@@ -7,5 +7,11 @@ CLIENT_IP=$(echo $SSH_CONNECTION | awk '{print $1}')
 logger -p local1.notice -t ssh-wrapper "User $USER from $CLIENT_IP executed '$SSH_ORIGINAL_COMMAND'"
 
 # Execute the requested command
-eval $SSH_ORIGINAL_COMMAND
+if [ -z "$SSH_ORIGINAL_COMMAND" ]
+then
+  # If no command was specified, start a shell
+  /bin/bash
+else
+  eval $SSH_ORIGINAL_COMMAND
+fi
 
